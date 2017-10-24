cdbe049cda758908d0ad668cd9ba17133625fe2a
Unknown <heg0001@suzannecoryhs.vic.edu.au>
2017-10-24 12:05:03 +1100

    Fixed some merging errors

--- core/src/tech/boxcubed/goblingame/GoblinGameMain.java
+++ core/src/tech/boxcubed/goblingame/GoblinGameMain.java
@@ -1,37 +1,35 @@
 package tech.boxcubed.goblingame;
 
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
-import tech.boxcubed.goblingame.screens.GameScreen;
-
-public class GoblinGameMain extends Game {
-	public SpriteBatch batch;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Formatter;
 import java.util.logging.Handler;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
-public class GoblinGameMain extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import tech.boxcubed.goblingame.screens.GameScreen;
+
+public class GoblinGameMain extends Game {
+	public SpriteBatch batch;
 	Logger logger;
 
 	public GoblinGameMain() {
 		logger=Logger.getLogger("GoblinGame");
+		
 		for (Handler handler : logger.getParent().getHandlers()) {
 			logger.getParent().removeHandler(handler);
 		}
+		
 		ConsoleHandler handler=new ConsoleHandler();
 		handler.setFormatter(new Formatter() {
 			@Override
 			public String format(LogRecord record) {
 				return record.getLevel()+": "+record.getMessage();
-
 			}
 		});
+		
 		logger.addHandler(handler);
 	}
 
@@ -39,7 +37,6 @@ public class GoblinGameMain extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		setScreen(new GameScreen(this));
-		img = new Texture("badlogic.jpg");
 		logger.info("Starting Goblin Game...");
 
 	}
@@ -52,6 +49,5 @@ public class GoblinGameMain extends ApplicationAdapter {
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
