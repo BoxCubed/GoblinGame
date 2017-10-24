4805c3f8442f198e7bdd5c9ef46983427d5b1ed1
Ryan Samarakoon <ryan2002.rs@gmail.com>
2017-10-24 11:42:15 +1100

    Added logger

--- core/src/tech/boxcubed/goblingame/GoblinGameMain.java
+++ core/src/tech/boxcubed/goblingame/GoblinGameMain.java
@@ -6,14 +6,39 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+
 public class GoblinGameMain extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	Logger logger;
+
+	public GoblinGameMain() {
+		logger=Logger.getLogger("GoblinGame");
+		for (Handler handler : logger.getParent().getHandlers()) {
+			logger.getParent().removeHandler(handler);
+		}
+		ConsoleHandler handler=new ConsoleHandler();
+		handler.setFormatter(new Formatter() {
+			@Override
+			public String format(LogRecord record) {
+				return record.getLevel()+": "+record.getMessage();
+
+			}
+		});
+		logger.addHandler(handler);
+	}
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		logger.info("Starting Goblin Game...");
+
 	}
 
 	@Override
@@ -23,6 +48,7 @@ public class GoblinGameMain extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+
 	}
 	
 	@Override
