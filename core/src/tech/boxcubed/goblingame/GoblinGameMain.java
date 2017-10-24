f9dc211024ca4612697aee0cef3a9a56759a0d26
Ryan Samarakoon <ryan2002.rs@gmail.com>
2017-10-24 14:46:03 +1100

    Better logger

--- core/src/tech/boxcubed/goblingame/GoblinGameMain.java
+++ core/src/tech/boxcubed/goblingame/GoblinGameMain.java
@@ -1,5 +1,13 @@
 package tech.boxcubed.goblingame;
 
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.Calendar;
+import java.util.Date;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Formatter;
 import java.util.logging.Handler;
@@ -17,19 +25,22 @@ public class GoblinGameMain extends Game {
 
 	public GoblinGameMain() {
 		logger=Logger.getLogger("GoblinGame");
-		
+
 		for (Handler handler : logger.getParent().getHandlers()) {
 			logger.getParent().removeHandler(handler);
 		}
-		
+
 		ConsoleHandler handler=new ConsoleHandler();
 		handler.setFormatter(new Formatter() {
 			@Override
 			public String format(LogRecord record) {
-				return record.getLevel()+": "+record.getMessage();
+				Calendar cal=Calendar.getInstance();
+				cal.setTimeInMillis(record.getMillis());
+				return "["+cal.get(Calendar.HOUR_OF_DAY)+":"+cal.get(Calendar.MINUTE)+":"+cal.get(Calendar.SECOND)+"] - "+record.getLevel()+": "+record.getMessage();
+
 			}
 		});
-		
+
 		logger.addHandler(handler);
 	}
 
